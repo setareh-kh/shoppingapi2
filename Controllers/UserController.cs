7e921809132a0967b86b5f11f17af89ef4a0ca88
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2024-01-17 14:24:28 +0330

    work on user controller and test CRUD and ok

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,23 +11,47 @@ namespace shoppingapi2.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UserController(IUserRepository userRepository,IMapper mapper)
+        public UserController(IUserRepository userRepository, IMapper mapper)
         {
-            _userRepository=userRepository;
-            _mapper=mapper;
+            _userRepository = userRepository;
+            _mapper = mapper;
         }
         [HttpPost]
-        [Route("Add")] 
+        [Route("Add")]
         public async Task<IActionResult> AddAsync([FromBody] AddUserDto addUserDto)
         {
-            var user=await _userRepository.AddAsync(addUserDto);
+            var user = await _userRepository.AddAsync(addUserDto);
             return Ok(_mapper.Map<UserUserDto>(user));
         }
         [HttpGet]
-        [Route("Get")] 
-        public async Task<IActionResult> GetById(int id)
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAllAsync()
         {
-            
+            var users = await _userRepository.GetAllAsync();
+            return Ok(users?.Count > 0 ? users.Select(x => _mapper.Map<UserUserDto>(x)) : "No Any exisit user");
         }
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+            return Ok(user != null ? _mapper.Map<UserUserDto>(user) : $"{id}number is not found!!");
+
+        }
+        [HttpPut]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, UpdateUserDto updateUserDto)
+        {
+            var result = await _userRepository.UpdateAsync(id, updateUserDto);
+            return Ok(result == true ? $"{id} number is updated" : $"{id}number is not found!!");
+        }
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var result = await _userRepository.DeleteAsync(id);
+            return Ok(result == true ? $"{id} number is deleted" : $"{id}number is not found!!");
+        }
+
     }
 }
