4b96fc9733839fb0de2104c129086e8e99423a8f
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2024-01-25 09:25:07 +0330

    no any change.test run ok

--- obj/Debug/net7.0/shoppingapi2.AssemblyInfo.cs
+++ obj/Debug/net7.0/shoppingapi2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shoppingapi2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577348210f3d8041b945393da6366a5fd753f6de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e7ce8e0d7e98880f3a385d5bcb08200af6f8bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("shoppingapi2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shoppingapi2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
